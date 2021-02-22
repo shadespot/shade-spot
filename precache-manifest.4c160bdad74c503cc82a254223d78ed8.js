6f839f43ed008197f0571f07f8528ceaf0efe61a
Andrew Stobie <agstobie@gmail.com>
2021-02-22 16:43:16 -0500

    Updates

---
+++ precache-manifest.4c160bdad74c503cc82a254223d78ed8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f55a48515a21a050554",
+    "url": "/shade-spot-rn/static/js/app.a1a3b09c.chunk.js"
+  },
+  {
+    "revision": "785ad99ea39fb2600158",
+    "url": "/shade-spot-rn/static/js/runtime~app.c5a26c67.js"
+  },
+  {
+    "revision": "2f613c30a3895fa368fd",
+    "url": "/shade-spot-rn/static/js/2.be743303.chunk.js"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/shade-spot-rn/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "4d9573b9cee872b8459664fc3c1e6ad7",
+    "url": "/shade-spot-rn/static/media/ShadeSpotLogoLight.2b5fc7f3.png"
+  },
+  {
+    "revision": "a0646f0ee2c9d2b0da5b9b43cf64c5ff",
+    "url": "/shade-spot-rn/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/shade-spot-rn/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/shade-spot-rn/expo-service-worker.js"
+  },
+  {
+    "revision": "cdffe9c5d7c254eb538145a36eae980a",
+    "url": "/shade-spot-rn/register-service-worker.js"
+  },
+  {
+    "revision": "1cca25f2e61cdb7dd28b898e81578b23",
+    "url": "/shade-spot-rn/static/js/2.be743303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02ee9497bd9b118f6bbe1fac80f35af",
+    "url": "/shade-spot-rn/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/shade-spot-rn/expo-service-worker.js",{scope:"/shade-spot-rn/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
