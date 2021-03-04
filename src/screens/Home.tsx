e716a15c1425d8f650d8a2565603d5b06863b634
Andrew Stobie <agstobie@gmail.com>
2021-03-04 03:05:53 -0500

    Add callback function to close modal after successful call in the child of modal

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -47,7 +47,7 @@ function Home() {
         isOpen={showModal}
         closeFn={handleHideSignupModal}
       >
-        <SignupForm />
+        <SignupForm returnFn={handleShowSignupModal} />
       </Modal>
     </View>
   );
